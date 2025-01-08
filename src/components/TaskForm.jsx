788122e1b973e256fa74bdf13864cf5bf00b3470
Amithasaa <amithaamitha290@gmail.com>
2025-01-08 12:14:26 +0530

    Added new ReactJS project

---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useState } from "react";
+
+export default function TaskForm({addTask}) {
+    
+            const [task, setTask] = useState('');
+            const [priority, setPriority] = useState("Medium");
+            const [category, setCategory] = useState("General");
+            const handlesubmit = (e) => {
+                e.preventDefault();
+                addTask({text: task, priority,category,completed: false}); // send data to add task
+                setPriority("medium");
+                setCategory('General');
+                setTask("");
+            }
+            return (
+                <form onSubmit={handlesubmit} className="task-form">
+                    <div>
+                        <input type="text" value={task} required onChange={(e) => setTask(e.target.value)}
+                        placeholder="Enter your task"></input>
+                        <button type="submit">Add Task</button>
+
+                    </div>
+                    <div id="btns">
+                        <select onChange={(e)=>setPriority(e.target.value)}>
+                            <option  value="High">High</option>
+                            <option  value="Medium">Medium</option>
+                            <option  value="Low">Low</option>
+                        </select>
+                        <select onChange={(e)=>setCategory(e.target.value)}>
+                            <option  value="General">General</option>
+                            <option  value="Work">Work</option>
+                            <option  value="Personal">Personal</option>
+                        </select>
+                        {/* <h3>{task}{priority}{category}</h3> */}
+                    </div>
+                </form>
+            )
+}
